d016ec4f1ba40b37272227979e29bccfc40cf8e8
Yuma Matsui <oshino.iiyo@gmail.com>
2023-11-05 18:30:34 +0900

    ✨ Route Groups done.

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+export default function Layout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section>
+      <h2>Marketing Layout</h2>
+      {children}
+    </section>
+  )
+}
---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,12 @@
+export default function Layout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section>
+      <h2>Shop Layout</h2>
+      {children}
+    </section>
+  )
+}
